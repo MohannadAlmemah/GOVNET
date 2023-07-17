cf7579c0f0183ede818a62ee52221bb04554ca2c
mohanad19977 <73375193+mohanad19977@users.noreply.github.com>
2023-07-17 13:26:39 +0300

    .

--- src/app/models/field.ts
+++ src/app/models/field.ts
@@ -1,12 +1,13 @@
 export class Field{
     public id:string;
     public type:string;
+    public designType:string;
     public regex:string|undefined;
     public editable:boolean|undefined;
     public required:boolean;
     public label:string;
     public hidden:boolean|undefined;
-    public value:string|undefined;
+    public value:any|undefined;
     public comment:string|undefined;
     public fields:Field[]|undefined;
     public multiSelect:boolean|undefined;
@@ -18,9 +19,9 @@ export class Field{
      */
     constructor(id:string,type:string,regex:string|undefined,
         editable:boolean|undefined,required:boolean,label:string,
-        hidden:boolean|undefined,value:string|undefined,comment:string|undefined,
+        hidden:boolean|undefined,value:any|undefined,comment:string|undefined,
         fields:Field[]|undefined,multiSelect:boolean|undefined,options:any[]|undefined,
-        comboBoxOptions:any[]|undefined,keyboardType:string
+        comboBoxOptions:any[]|undefined,keyboardType:string,designType:string
         ) {
             this.id=id;
             this.type=type;
@@ -36,5 +37,6 @@ export class Field{
             this.options=options;
             this.comboBoxOptions=comboBoxOptions;
             this.keyboardType=keyboardType;
+            this.designType=designType;
         }
 }
